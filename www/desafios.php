f7e500464a4917574878c6fd7cd08c0e5b0a09f4
ProXySec <ondproxy@gmail.com>
2017-10-03 22:39:36 -0300

    EhLab 3

--- www/desafios.php
+++ www/desafios.php
@@ -61,11 +61,11 @@ session_start();
 					$st = $con->prepare("SELECT * FROM `score`");
 					$st->execute();
 					$n = $st->fetchObject();
-					$score = (int) $n->pass + $n->file + $n->true_f;
+					$score = (int) $n->pass + $n->file + $n->true_f + $n->firewall;
 				?>
 					<h2>Score</h2>
 						<li><p>Score atual : <b><?php echo $score;?> pontos</b></p></li>
-						<li><p>Score máximo : <b>100 pontos</b></p></li>
+						<li><p>Score máximo : <b>150 pontos</b></p></li>
 					<h2>Desafios</h2>
 						<li><p>Senha do administrador (nível : fácil)</p></li>
 						<a href="#" class="pass">Adicionar flag</a>
@@ -73,7 +73,7 @@ session_start();
 						<span class="close" id="close_pass">X</span>
 							<form method="post" enctype="multipart/form-data">
 								<h3>Flag : Senha do administrador</h3>
-								<img src="http://www.iconsdb.com/icons/preview/orange/key-6-xxl.png" width="100">
+								<img src="key.png" width="100">
 								<input type="password" name="password" placeholder="Flag aqui"><br>
 								<input type="submit" name="send1" value="Enviar">
 							</form>
@@ -84,21 +84,33 @@ session_start();
 						<span class="close" id="close_file">X</span>
 							<form method="post" enctype="multipart/form-data">
 								<h3>Flag : Criar arquivo</h3>
-								<img src="http://www.iconsdb.com/icons/preview/orange/key-6-xxl.png" width="100"><br>
+								<img src="key.png" width="100"><br>
 								<code>home/own3d_flag.txt</code>
 								<input type="submit" name="send2" value="Validar arquivo">
 							</form>
 						</div>
-						<li><p>True Flag (nível : difícil)</p></li>
+						<li><p>True Flag (nível : médio)</p></li>
 						<a href="#" class="true">Adicionar flag</a>
 						<div id="flag_true">
 						<span class="close" id="close_true">X</span>
 							<form method="post" enctype="multipart/form-data">
 								<h3>Flag : Flag True</h3>
-								<img src="http://www.iconsdb.com/icons/preview/orange/key-6-xxl.png" width="100">
+								<img src="key.png" width="100">
 								<input type="password" name="true_f" placeholder="Flag aqui"><br>
 								<input type="submit" name="send3" value="Enviar">
 							</form>
+						</div>
+						<div class="right_div">
+							<li><p>Desabilitar firewall (nível : difícil)</p></li>
+							<a href="#" class="firewall" id="right_a">Adicionar flag</a>
+							<div id="flag_friewall">
+							<span class="close" id="close_firewall">X</span>
+								<form method="post" enctype="multipart/form-data">
+									<h3>Flag : Flag True</h3>
+									<img src="key.png" width="100">
+									<input type="submit" name="send4" value="Validar firewall">
+								</form>
+							</div>
 						</div>
 				</ul>
 			</div>
@@ -106,6 +118,7 @@ session_start();
 <script type="text/javascript">
 // não mexe nessa porra
 $(document).ready(function(){$("#close_pass").click(function (){$("#flag_pass").toggle("slow");});$("#close_file").click(function (){$("#flag_file").toggle("slow");});$("#close_true").click(function (){$("#flag_true").toggle("slow");});$(".pass").click(function(){$("#flag_pass").toggle("slow");});$(".file").click(function(){$("#flag_file").toggle("slow");});$(".true").click(function(){$("#flag_true").toggle("slow");});});
+$(document).ready(function(){$("#close_firewall").click(function(){$("#flag_friewall").toggle("slow");});$(".firewall").click(function(){$("#flag_friewall").toggle("slow");});});
 </script>
 		<?php 
 			$error_flag = '<script>alert("Flag inválida !\r\nVocê não pontuou");</script>';
@@ -137,7 +150,7 @@ $(document).ready(function(){$("#close_pass").click(function (){$("#flag_pass").
 				// flag file
 				$flag = "login/home/own3d_flag.txt";
 				if(is_file($flag) && file_exists($flag)):
-					if(upd_score($con, "file", 30) == true):
+					if(upd_score($con, "file", 35) == true):
 						echo $success_flag;
 					else:
 						echo $error_flag;
@@ -150,7 +163,7 @@ $(document).ready(function(){$("#close_pass").click(function (){$("#flag_pass").
 				$true = "#Th1sM4yB3f_l4g.estegonografia";
 				$enter_flag = filter_input(INPUT_POST, "true_f", FILTER_SANITIZE_SPECIAL_CHARS);
 				if($enter_flag == $true):
-					if(upd_score($con, "true_f", 50) == true):
+					if(upd_score($con, "true_f", 30) == true):
 						echo $success_flag;
 					else:
 						echo $error_flag;
@@ -158,8 +171,41 @@ $(document).ready(function(){$("#close_pass").click(function (){$("#flag_pass").
 				else:
 					echo $error_flag;
 				endif;
-
 			endif;	
+			if(isset($_POST['send4'])):
+				// flag firewall disable
+				$uname = php_uname(); // executa o uname -a
+				$split = explode(" ", $uname); // delemita a saída do uname e retorna uma array
+				$os = $split[0]; // sistema operacional
+				// firewall disable chalenger
+				if($os == "Windows"):
+					// WINDOWS 10
+					$verifica_firewall = "netsh advfirewall show currentprofile > null"; // comando de verificação de firewall windows
+					system($verifica_firewall); // executa o comando
+					$arquivo_ver = file_get_contents("null"); // arquivo onde a saida do comando foi salva
+					$split_ver = explode(" ", $arquivo_ver); // dividi as linhas do arquivo em uma array
+					$split_ver_1 = explode("\r\n", $split_ver[35]); // array onde está o estado do firewall em pt-BR
+					$estado_firewall = $split_ver_1[0]; // atual estado do firewall no Windows 10 pt-BR
+					if($estado_firewall != "Ligado"):
+						upd_score($con, "firewall", 65);
+						echo $success_flag;
+					else:
+						echo $error_flag;
+					endif;
+					system("del null");
+				elseif($os == "Linux"):
+					$verifica_firewall = "iptable -L > null";
+					system($verifica_firewall);
+					$arquivo_ver = file_get_contents("null");
+					if($arquivo_ver == "" || $arquivo_ver == "\n" || $arquivo_ver == "\r" || $arquivo_ver == "\r\n"):
+						upd_score($con, "firewall", 65);
+						echo $success_flag;
+					else:
+						echo $error_flag;
+					endif;
+					system("del null");
+				endif;
+			endif;
 		?>
 	</section>
 </section>
